660c422bac58391981f81f27747058031823960a
hwangblood <hwangblood@qq.com>
2022-10-26 13:24:05 +0800

    Chatper 2: Building a recipe list

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'recipe.dart';
+
 void main() {
   runApp(const RecipeApp());
 }
@@ -49,10 +51,40 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        // TODO: Replace child: Container()
-        child: Container(),
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            // TODO: Add GestureDetector
+            return buildRecipeCard(Recipe.samples[index]);
+          },
+        ),
       ),
     );
-    // TODO: Add buildRecipeCard() here
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
