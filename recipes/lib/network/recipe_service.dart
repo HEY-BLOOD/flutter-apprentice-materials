9be5c08cb9694708e6539fcc09de618dcd4a041d
hwangblood <hwangblood@qq.com>
2022-11-09 20:56:34 +0800

    Chapter 11: Connecting to the recipe service

---
+++ recipes/lib/network/recipe_service.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+import 'package:http/http.dart';
+
+const String apiKey = '35abbbb8d99cd62a15a95a4b227efc8f';
+const String apiId = '832a5f8e';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  Future getData(String url) async {
+    final response = await get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      log(response.body);
+    }
+  }
+
+  Future<dynamic> getRecipes(String query, int from, int to) async {
+    final recipeData = await getData(
+        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
+    return recipeData;
+  }
+}
