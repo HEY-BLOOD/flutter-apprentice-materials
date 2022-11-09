cd10c53cc68c4fee553e1bb115146b0eaeff6742
hwangblood <hwangblood@qq.com>
2022-11-09 16:29:41 +0800

    Chapter 7: Logging Out

--- fooderlich/lib/screens/profile_screen.dart
+++ fooderlich/lib/screens/profile_screen.dart
@@ -59,7 +59,7 @@ class ProfileScreenState extends State<ProfileScreen> {
         ListTile(
           title: const Text('Log out'),
           onTap: () {
-            // TODO: Logout user
+            Provider.of<AppStateManager>(context, listen: false).logout();
           },
         )
       ],
