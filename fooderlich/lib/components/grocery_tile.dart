f70acbcd05bce4124bf695976da08eb4a6c9ff4a
hwangblood <hwangblood@qq.com>
2022-11-09 12:59:09 +0800

    Chapter 6: Finishing GroceryTile

--- fooderlich/lib/components/grocery_tile.dart
+++ fooderlich/lib/components/grocery_tile.dart
@@ -21,8 +21,42 @@ class GroceryTile extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // TODO: Add Row to group (name, date, importance)
-          // TODO: Add Row to group (quantity, checkbox)
+          Row(
+            children: [
+              Container(width: 5.0, color: item.color),
+              const SizedBox(width: 16.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: GoogleFonts.lato(
+                      decoration: textDecoration,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  buildDate(),
+                  const SizedBox(height: 4.0),
+                  buildImportance(),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                  decoration: textDecoration,
+                  fontSize: 21.0,
+                ),
+              ),
+              buildCheckbox(),
+            ],
+          ),
         ],
       ),
     );
