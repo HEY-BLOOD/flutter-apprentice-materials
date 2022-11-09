e0b9da31f0a2bb98c733b3e08784d8afc4264d19
hwangblood <hwangblood@qq.com>
2022-11-09 11:55:20 +0800

    Chapter 6: Managing the grocery items

--- fooderlich/lib/screens/grocery_screen.dart
+++ fooderlich/lib/screens/grocery_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/models.dart';
 import 'empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    // TODO: Add a scaffold widget
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO: Present GroceryItemScreen
+        },
+      ),
+      body: buildGroceryScreen(),
+    );
   }
-  // TODO: Add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(
+      builder: (context, manager, child) {
+        if (manager.groceryItems.isNotEmpty) {
+          // TODO: Add GroceryListScreen
+          return Container();
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
 }
