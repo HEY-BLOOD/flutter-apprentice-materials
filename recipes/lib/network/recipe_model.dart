f466df134ae36b1cd033164a16684d58422375a2
hwangblood <hwangblood@qq.com>
2022-11-09 20:27:29 +0800

    Chapter 10: Creating model classes

---
+++ recipes/lib/network/recipe_model.dart
@@ -0,0 +1,11 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'recipe_model.g.dart';
+
+@JsonSerializable()
+class APIRecipeQuery {
+// TODO: Add APIRecipeQuery.fromJson
+}
+// TODO: Add @JsonSerializable() class APIHits
+// TODO: Add @JsonSerializable() class APIRecipe
+// TODO: Add @JsonSerializable() class APIIngredients
