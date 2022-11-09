9eb78ed7cb1f90d3406230958978b167b72af9dc
hwangblood <hwangblood@qq.com>
2022-11-09 22:19:48 +0800

    Chapter 12: Encoding and decoding JSON

--- recipes/lib/network/model_converter.dart
+++ recipes/lib/network/model_converter.dart
@@ -17,8 +17,38 @@ class ModelConverter implements Converter {
     return encodeJson(req);
   }
 
-  Request encodeJson(Request request) {}
-  Response decodeJson<BodyType, InnerType>(Response response) {}
+  Request encodeJson(Request request) {
+    final contentType = request.headers[contentTypeKey];
+    if (contentType != null && contentType.contains(jsonHeaders)) {
+      return request.copyWith(body: json.encode(request.body));
+    }
+    return request;
+  }
+
+  Response<BodyType> decodeJson<BodyType, InnerType>(Response response) {
+    final contentType = response.headers[contentTypeKey];
+    var body = response.body;
+    if (contentType != null && contentType.contains(jsonHeaders)) {
+      body = utf8.decode(response.bodyBytes);
+    }
+    try {
+      final mapData = json.decode(body);
+      if (mapData['status'] != null) {
+        return response.copyWith<BodyType>(
+            body: Error(Exception(mapData['status'])) as BodyType);
+      }
+      final recipeQuery = APIRecipeQuery.fromJson(mapData);
+      return response.copyWith<BodyType>(
+          body: Success(recipeQuery) as BodyType);
+    } catch (e) {
+      chopperLogger.warning(e);
+      return response.copyWith<BodyType>(
+          body: Error(e as Exception) as BodyType);
+    }
+  }
+
   @override
-  Response<BodyType> convertResponse<BodyType, InnerType>(Response response) {}
+  Response<BodyType> convertResponse<BodyType, InnerType>(Response response) {
+    return decodeJson<BodyType, InnerType>(response);
+  }
 }
